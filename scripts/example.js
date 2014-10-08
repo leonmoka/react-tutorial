298353ad67f9918a28c85fccce109eb16966714b
luca <luca.mirra@etalia.net>
2014-10-08 16:35:32 +0200

    first commit

---
+++ scripts/example.js
@@ -0,0 +1,214 @@
+/** @jsx React.DOM */
+
+// ### The hierarchy Structure of components: 
+// - CommentBox
+//   - CommentList
+//     - Comment
+//   - CommentForm
+
+var converter = new Showdown.converter();
+var data = [
+  {author: "Leoncino", text: "This bull terrier is a puppy"},
+  {author: "Nonno Lio", text: "This is the old one"}
+];
+
+var CommentBox = React.createClass({displayName: 'CommentBox',
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Hello, moka! Your dog is a the bull terrier</h1>
+        <CommentList data={this.props.data}/>
+        <CommentForm />
+      </div>
+    );
+  }
+});
+// <h2>{this.props.data}</h2>
+
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Ciao questa è commentForm
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: converter.makeHtml(this.props.children.toString())}} />
+        <br />
+      </div>
+    );
+  }
+});
+        // {this.props.children}
+        // {converter.makeHtml(this.props.children.toString())}
+
+React.renderComponent(
+  <CommentBox data={data} />,
+  document.getElementById('content')
+);
+
+// React.renderComponent(
+//   CommentList(null),
+//   document.getElementById("first-list")
+// );
+
+// React.renderComponent(
+//   CommentForm(null),
+//   document.getElementById("first-form")
+// );
+
+
+
+
+
+
+
+
+
+
+// /** @jsx React.DOM */
+
+// var converter = new Showdown.converter();
+
+// var Comment = React.createClass({
+//   render: function() {
+//     var rawMarkup = converter.makeHtml(this.props.children.toString());
+
+//     return (
+//       <div className="comment">
+//         <h2 className="commentAuthor">
+//           {this.props.author}
+//         </h2>
+//         <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+//       </div>
+//     );
+//   }
+// });
+
+// var CommentBox = React.createClass({
+//   loadCommentsFromServer: function() {
+//     $.ajax({
+//       url: this.props.url,
+//       dataType: 'json',
+//       success: function(data) {
+//         this.setState({data: data});
+//       }.bind(this),
+//       error: function(xhr, status, err) {
+//         console.error(this.props.url, status, err.toString());
+//       }.bind(this)
+//     });
+//   },
+//   handleCommentSubmit: function(comment) {
+//     var comments = this.state.data;
+//     comments.push(comment);
+//     this.setState({data: comments}, function() {
+//       // `setState` accepts a callback. To avoid (improbable) race condition,
+//       // `we'll send the ajax request right after we optimistically set the new
+//       // `state.
+//       $.ajax({
+//         url: this.props.url,
+//         dataType: 'json',
+//         type: 'POST',
+//         data: comment,
+//         success: function(data) {
+//           this.setState({data: data});
+//         }.bind(this),
+//         error: function(xhr, status, err) {
+//           console.error(this.props.url, status, err.toString());
+//         }.bind(this)
+//       });
+//     });
+//   },
+//   getInitialState: function() {
+//     return {data: []};
+//   },
+//   componentDidMount: function() {
+//     this.loadCommentsFromServer();
+//     setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+//   },
+//   render: function() {
+//     return (
+//       <div className="commentBox">
+//         <h1>Comments</h1>
+//         <CommentList data={this.state.data} />
+//         <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+//       </div>
+//     );
+//   }
+// });
+
+// var CommentList = React.createClass({
+//   render: function() {
+//     var commentNodes = this.props.data.map(function(comment, index) {
+//       return (
+//         // `key` is a React-specific concept and is not mandatory for the
+//         // purpose of this tutorial. if you're curious, see more here:
+//         // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
+//         <Comment author={comment.author} key={index}>
+//           {comment.text}
+//         </Comment>
+//       );
+//     });
+//     return (
+//       <div className="commentList">
+//         {commentNodes}
+//       </div>
+//     );
+//   }
+// });
+
+// var CommentForm = React.createClass({
+//   handleSubmit: function(e) {
+//     e.preventDefault();
+//     var author = this.refs.author.getDOMNode().value.trim();
+//     var text = this.refs.text.getDOMNode().value.trim();
+//     if (!text || !author) {
+//       return;
+//     }
+//     this.props.onCommentSubmit({author: author, text: text});
+//     this.refs.author.getDOMNode().value = '';
+//     this.refs.text.getDOMNode().value = '';
+//     return;
+//   },
+//   render: function() {
+//     return (
+//       <form className="commentForm" onSubmit={this.handleSubmit}>
+//         <input type="text" placeholder="Your name" ref="author" />
+//         <input type="text" placeholder="Say something..." ref="text" />
+//         <input type="submit" value="Post" />
+//       </form>
+//     );
+//   }
+// });
+
+// React.renderComponent(
+//   <CommentBox url="comments.json" pollInterval={2000} />,
+//   document.getElementById('content')
+// );
